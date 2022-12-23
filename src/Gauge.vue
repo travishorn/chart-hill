49a5f63f35768b551f4816681e0e52e6670aaa00
Travis Horn <travis@travishorn.com>
2022-12-22 19:59:40 -0600

    Separate prop updates

--- src/Gauge.vue
+++ src/Gauge.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { onMounted, watch } from "vue";
-import { min, max } from "d3-array";
 import { scaleLinear } from "d3-scale";
 import { select } from "d3-selection";
 import { arc } from "d3-shape";
@@ -20,7 +19,7 @@ const props = defineProps({
   },
   barColor: {
     type: String,
-    default: "mediumseagreen",
+    default: "#106cde",
   }
 });
 
@@ -33,75 +32,107 @@ const barWidth = height * 0.33;
 const fontSize = 50;
 const scale = scaleLinear([degToRad(-90), degToRad(90)]);
 let chart;
+let guideBar;
+let valueBar;
+let labelMin;
+let labelMax;
+let labelValue;
 
-const update = () => {
-  chart.selectAll("path.valueBar")
+const valueArc = arc()
+  .innerRadius(height - fontSize - barWidth)
+  .outerRadius(height - fontSize - 10)
+  .startAngle(scale(0))
+  .endAngle(() => {
+    const pct = (props.value - props.min) / (props.max - props.min);
+    if (pct < 0) return scale(0);
+    if (pct > 1) return scale(1);
+    return scale(pct);
+  });
+
+onMounted(() => {
+  chart = select(`#${chartId}`);
+
+  valueBar = chart.selectAll("path.valueBar")
     .data([props.value])
     .join("path")
       .attr("class", "valueBar")
-      .attr("d", arc()
-        .innerRadius(height - fontSize - barWidth)
-        .outerRadius(height - fontSize - 10)
-        .startAngle(scale(0))
-        .endAngle(scale(max([min([(props.value - props.min) / (props.max - props.min), 1]), 0])))
-      )
       .attr("transform", `translate(${width / 2}, ${height - fontSize})`)
       .attr("fill", props.barColor);
-  
-  chart.selectAll("path.maxBar")
+
+  labelMin = chart.selectAll("text.label-min")
+    .data([props.min])
+    .join("text")
+      .attr("class", "label-min")
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", fontSize)
+      .attr("fill", "currentColor")
+      .attr(
+        "transform",
+        `translate(${barWidth / 2 + fontSize} ${height - 5})`
+      );
+
+  labelMax = chart.selectAll("text.label-max")
+    .data([props.max])
+    .join("text")
+      .attr("class", "label-max")
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", fontSize)
+      .attr("fill", "currentColor")
+      .attr(
+        "transform",
+        `translate(${width - (barWidth / 2 + fontSize)} ${height - 5})`
+      );
+
+  labelValue = chart.selectAll("text.label-value")
+    .data([props.value])
+    .join("text")
+      .attr("class", "label-value")
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", fontSize * 3)
+      .attr("fill", "currentColor")
+      .attr("transform", `translate(${width / 2} ${height / 2 + barWidth})`);
+
+  guideBar = chart.selectAll("path.guideBar")
     .data([props.max])
     .join("path")
-      .attr("class", "maxBar")
+      .attr("class", "guideBar")
       .attr("d", arc()
         .innerRadius(height - fontSize - 5)
         .outerRadius(height - fontSize)
         .startAngle(scale(0))
         .endAngle(scale(1))
       )
-      .attr("transform", `translate(${width / 2}, ${height - fontSize})`)
-      .attr("fill", props.barColor);
+      .attr("transform", `translate(${width / 2}, ${height - fontSize})`);
 
-  chart.selectAll("text.label-min")
-    .data([props.min])
-    .join("text")
-      .attr("class", "label-min")
-      .text(d => d)
-      .attr("text-anchor", "middle")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", fontSize)
-      .attr("fill", "currentColor")
-      .attr("transform", `translate(${barWidth / 2 + fontSize} ${height - 5})`);
-
-  chart.selectAll("text.label-max")
-    .data([props.max])
-    .join("text")
-      .attr("class", "label-max")
-      .text(d => d)
-      .attr("text-anchor", "middle")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", fontSize)
-      .attr("fill", "currentColor")
-      .attr("transform", `translate(${width - (barWidth / 2 + fontSize)} ${height - 5})`);
-
-  chart.selectAll("text.label-value")
-    .data([props.value])
-    .join("text")
-      .attr("class", "label-value")
-      .text(d => d)
-      .attr("text-anchor", "middle")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", fontSize * 3)
-      .attr("fill", "currentColor")
-      .attr("transform", `translate(${width / 2} ${height / 2 + barWidth})`);
-};
-
-onMounted(() => {
-  chart = select(`#${chartId}`);
-  update();
+  guideBar.attr("fill", props.barColor);
+  valueBar.attr("fill", props.barColor);
+  valueBar.attr("d", valueArc);
+  labelMin.text(props.min);
+  labelMax.text(props.max);
+  labelValue.text(props.value);
 });
 
-watch(() => [props.min, props.max, props.value, props.barColor], () =>{
-  update();
+watch(() => props.barColor, () => {
+  guideBar.attr("fill", props.barColor);
+  valueBar.attr("fill", props.barColor);
+});
+
+watch(() => props.value, () => {
+  valueBar.attr("d", valueArc);
+  labelValue.text(props.value);
+});
+
+watch(() => props.min, () => {
+  valueBar.attr("d", valueArc);
+  labelMin.text(props.min);
+});
+
+watch(() => props.max, () => {
+  valueBar.attr("d", valueArc);
+  labelMax.text(props.max);
 });
 </script>
 
